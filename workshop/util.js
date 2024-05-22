3b679a2bc8599a36b8bd2ea8fcf0226b8b2fde44
Nikolaj Ignatieff Schwartzbach <nschwartzbach@hotmail.com>
2024-05-22 14:33:36 +0200

    added federico abstract

--- workshop/util.js
+++ workshop/util.js
@@ -53,18 +53,18 @@ function time(){
 
 	c.innerHTML = timeNow();
 
-	dref.setHours(9,0,0,0);
-	var order = ["coffee1","giulio_b",	"cengiz",	"coffee2",	"matthieu",	"florent", 	"lunch",	"zohar",	"song",	"coffee3",	"yue", "sueyeon",		"dinner"];
-	var times = [9, 		9.5, 		10.25, 		11, 		11.5, 	    12.25, 		13, 		14.5, 		15.25,		15.75, 		16.5, 	 17.25,	     	20];
+	dref.setHours(9,30,0,0);
+	var order = ["giulio_b",	"cengiz",	"coffee2",	"matthieu",	"florent", 	"lunch",	"zohar",	"song",	"coffee3",	"yue", "sueyeon",		"dinner"];
+	var times = [9.5, 		10.25, 		11, 		11.5, 	    12.25, 		13, 		14.5, 		15.25,		15.75, 		16.5, 	 17.25,	     	20];
 	if(order.length != times.length){
 		alert("order length ("+order.length+") != times length ("+times.length+")");
 	}
-	if(d.getHours() >= 9 && d.getHours() < 22){
+	if(d.getHours() >= 9.5 && d.getHours() < 22){
 		var hrs = (d-dref)/1000/60/60;
 		var slot = "";
 		var i_ = -1;
 		for(var i=1; i<order.length; i++){
-			if(times[i] > hrs + 9){
+			if(times[i] > hrs + 9.5){
 				slot = order[i-1];
 				i_ = i;
 				break;
@@ -72,27 +72,27 @@ function time(){
 		}
 		// evil hacks to calibrate the line (hopefully it works in other browsers than chrome)
 		var y_pos;
-		if(hrs < 18 - 9){
-			if(hrs >= 17.25 - 9){
+		if(hrs < 18 - 9.5){
+			if(hrs >= 17.25 - 9.5){
 				var elem = document.getElementById("td_sueyeon");
 				var y_top = 5+elem.getBoundingClientRect().top - document.body.getBoundingClientRect().top,
 				dy = elem.getBoundingClientRect().bottom - y_top  - document.body.getBoundingClientRect().top;
-				y_pos = y_top + (hrs + 9 - 16.5) / 0.75 * dy; 
+				y_pos = y_top + (hrs + 9.5 - 16.5) / 0.75 * dy; 
 			} else {
 				var elem = document.getElementById("td_"+slot);
 				var y_top = elem.getBoundingClientRect().top - document.body.getBoundingClientRect().top,
 					dy = elem.getBoundingClientRect().bottom - y_top  - document.body.getBoundingClientRect().top;
-				y_pos = y_top + (hrs + 9 - times[i_-1]) / (times[i_] - times[i_-1]) * dy;
+				y_pos = y_top + (hrs + 9.5 - times[i_-1]) / (times[i_] - times[i_-1]) * dy;
 			}
-		} else if(hrs < (20 - 9)) {
+		} else if(hrs < (20 - 9.5)) {
 			var y_top = document.getElementById("td_sueyeon").getBoundingClientRect().bottom - document.body.getBoundingClientRect().top,
 			    y_bottom = 5+document.getElementById("td_dinner").getBoundingClientRect().top - document.body.getBoundingClientRect().top;
-			y_pos = y_top + (y_bottom - y_top) * ((hrs - (17.25 - 9)) / (20-17.25));
+			y_pos = y_top + (y_bottom - y_top) * ((hrs - (17.25 - 9.5)) / (20-17.25));
 		} else  {
 			var elem = document.getElementById("td_dinner");
 			var y_top = 5+elem.getBoundingClientRect().top - document.body.getBoundingClientRect().top,
 				dy = elem.getBoundingClientRect().bottom - y_top  - document.body.getBoundingClientRect().top;
-			y_pos = y_top + (hrs + 9 - 20) / (22 - 20) * dy;
+			y_pos = y_top + (hrs + 9.5 - 20) / (22 - 20) * dy;
 		}
 		c.style.top = (y_pos-8)+"px";
 		l.style.top = y_pos+"px";
